5ba8cf932c8124c59799fe0bc0719199f26d1fdd
Muriziq <murizikthanni@gmail.com>
2026-01-25 16:38:40 +0100

    retouch animation

--- script.js
+++ script.js
@@ -21,35 +21,6 @@ if(mq.matches){
 clip = `M 0,30 A 30,30,0,0,1 30,0 L ${artSec.width - maxes.width},0   L ${(artSec.width - maxes.width) + 100},${maxes.height} L ${artSec.width - 30},${maxes.height} A 30,30,0,0,1 ${artSec.width},100 L ${artSec.width},${artSec.height-30} A 30,30,0,0,1 ${artSec.width - 30},${artSec.height} L ${maxes.width},${artSec.height} L ${(maxes.width) - 100},${artSec.height - maxes.height} L 30,${artSec.height - maxes.height} A 30,30,0,0,1 0,${artSec.height - maxes.height - 30}  Z`
 }
 article.style.clipPath = `path("${clip}")`
-        const herotl = gsap.timeline()
-if(mq.matches){
-    herotl.fromTo(".firstmain article > img",{opacity:0},{opacity:1,duration:1})
-    herotl.fromTo(".firstd",{opacity:0,x:80},{opacity:1,x:0,duration:0.7},"-=0.5")
-        herotl.fromTo(".f1",{opacity:0,y:80},{opacity:1,y:0,duration:0.7},"-=0.5")
-            herotl.fromTo(".f2",{opacity:0,x:80},{opacity:1,x:0,duration:0.7},"-=0.5")
-                    herotl.fromTo(".second1",{opacity:0,y:80},{opacity:1,y:0,duration:0.7},"-=0.5")
-}else{
-        const herotl = gsap.timeline()
-    herotl.fromTo(".firstmain article > img",{opacity:0},{opacity:1,duration:1})
-        herotl.fromTo(".second1",{opacity:0,y:80},{opacity:1,y:0,duration:0.7},"-=0.5")
-    herotl.fromTo(".firstd",{opacity:0,y:80},{opacity:1,y:0,duration:0.7},"-=0.5")
-        herotl.fromTo(".f1",{opacity:0,y:80},{opacity:1,y:0,duration:0.7},"-=0.5")
-            herotl.fromTo(".f2",{opacity:0,x:80},{opacity:1,x:0,duration:0.7},"-=0.5")
-        }
-        herotl.fromTo(".choose > div",{opacity:0,y:80},{opacity:1,y:0})
-  const herotl2 = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".firstmain",
-      start: `top top`,
-      end: "bottom top",
-      scrub: true
-    }
-  });
-  herotl2.fromTo(".firstmain article > img", {y:0, opacity:1}, {y:-100, opacity:0});
-  herotl2.fromTo(".firstd", {y:0, opacity:1}, {y:-150, opacity:0},"<");
-  herotl2.fromTo(".second1", {y:0, opacity:1}, {y:-100, opacity:0},"<");
-  herotl2.fromTo(".f1", {y:0, opacity:1}, {y:-150, opacity:0},"<");
-  herotl2.fromTo(".f2", {y:0, opacity:1}, {y:-150, opacity:0},"<");
 
 }
 
@@ -88,7 +59,37 @@ function hotDeals(mq){
         },2500)
     },3000)
 }
-function gsapAnimations(){
+function gsapAnimations(mq){
+        const herotl = gsap.timeline()
+if(mq.matches){
+    herotl.fromTo(".firstmain article > img",{opacity:0},{opacity:1,duration:1})
+    herotl.fromTo(".firstd",{opacity:0,x:80},{opacity:1,x:0,duration:0.7},"-=0.5")
+        herotl.fromTo(".f1",{opacity:0,y:80},{opacity:1,y:0,duration:0.7},"-=0.5")
+            herotl.fromTo(".f2",{opacity:0,x:80},{opacity:1,x:0,duration:0.7},"-=0.5")
+                    herotl.fromTo(".second1",{opacity:0,y:80},{opacity:1,y:0,duration:0.7},"-=0.5")
+}else{
+        const herotl = gsap.timeline()
+    herotl.fromTo(".firstmain article > img",{opacity:0},{opacity:1,duration:1})
+        herotl.fromTo(".second1",{opacity:0,y:80},{opacity:1,y:0,duration:0.7},"-=0.5")
+    herotl.fromTo(".firstd",{opacity:0,y:80},{opacity:1,y:0,duration:0.7},"-=0.5")
+        herotl.fromTo(".f1",{opacity:0,y:80},{opacity:1,y:0,duration:0.7},"-=0.5")
+            herotl.fromTo(".f2",{opacity:0,x:80},{opacity:1,x:0,duration:0.7},"-=0.5")
+        }
+        herotl.fromTo(".choose > div",{opacity:0,y:80},{opacity:1,y:0})
+  const herotl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".firstmain",
+      start: `top top`,
+      end: "bottom top",
+      scrub: true
+    }
+  });
+  herotl2.fromTo(".firstmain article > img", {y:0, opacity:1}, {y:-100, opacity:0});
+  herotl2.fromTo(".firstd", {y:0, opacity:1}, {y:-150, opacity:0},"<");
+  herotl2.fromTo(".second1", {y:0, opacity:1}, {y:-100, opacity:0},"<");
+  herotl2.fromTo(".f1", {y:0, opacity:1}, {y:-150, opacity:0},"<");
+  herotl2.fromTo(".f2", {y:0, opacity:1}, {y:-150, opacity:0},"<");
+
  document.querySelectorAll(".choose > section > div").forEach(div=>{
     const divtl = gsap.timeline({scrollTrigger:{trigger: div, start:"top 90%", end:"bottom 60%", scrub:true}})
     divtl.fromTo(div,{opacity:0,y:150},{opacity:1,y:0,duration:0.7})
@@ -123,7 +124,7 @@ formstl.fromTo(".forms > section > div",{opacity:0,y:150},{opacity:1,y:0,duratio
 window.addEventListener("DOMContentLoaded",()=>{
     heroBg(mq)
     hotDeals(mq)
-gsapAnimations()
+gsapAnimations(mq)
 
 
 
